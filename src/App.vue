21a2c2b98f6f5416e70defe6692ff51efe1f1131
tobygoetz <goetz.tobiah@student.greenriver.edu>
2024-04-28 15:55:07 -0700

    implemented pages and updated main.js to use "vue-router"

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello from Vue</h1>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/pages/ProductsPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Products Page</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'ProductsPage',
+}
+</script>
